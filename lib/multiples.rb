70fe3000f0b5817043bbd0db170b57f4ef8664ad
jmheyd <heydjm@comcast.net>
2015-12-21 18:46:51 -0600

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,22 @@
-# Enter your procedural solution here!
+# Enter your procedural solution here
+
+  def collect_multiples(limit)
+    multiples_array = []
+    i=1
+    while i < limit
+      if i % 3 == 0 || i % 5 == 0 then
+        multiples_array.push(i)
+      end
+        i += 1
+    end
+    return multiples_array
+  end
+
+
+  def sum_multiples(limit)
+    collect_multiples(limit).inject(:+)
+  end
+
+  #sum_multiples(10)
+
+  
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,27 @@
 # Enter your object-oriented solution here!
+class Multiples
+
+  def initialize(limit, multiples_array = nil)
+    @limit = limit
+    @multiples_array = multiples_array || Array.new
+  end
+
+  def collect_multiples
+    i=1
+    while i < @limit
+      if i % 3 == 0 || i % 5 == 0 then
+       @multiples_array.push(i)
+      end
+        i += 1
+      end
+      @multiples_array.compact
+    return @multiples_array.compact
+  end
+
+
+  def sum_multiples
+    collect_multiples.inject(:+)
+  end
+
+end
+
